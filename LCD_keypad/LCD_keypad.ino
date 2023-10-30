5252c6b7188beaaeed8c2fce96c1f9989319e202
PinkTultle <inseuk1007@naver.com>
2023-10-30 21:28:44 +0900

    메인 스케치 이름 변경 및 코드 이름 순서 재 정렬

--- LCD_keypad/LCD_keypad.ino
+++ LCD_keypad/LCD_keypad.ino
@@ -3,31 +3,48 @@
 
 LCD_I2C lcd(0x27, 16, 2); // Default address of most PCF8574 modules, change according
 
+uint8_t ho[8] =
+{
+    0b00100,
+    0b11111,
+    0b00000,
+    0b01110,
+    0b10001,
+    0b01110,
+    0b00100,
+    0b11111,
+};
+
+
 void setup()
 {
     lcd.begin(); // If you are using more I2C devices using the Wire library use lcd.begin(false)
                  // this stop the library(LCD_I2C) from calling Wire.begin()
     lcd.backlight();
+
+    lcd.createChar(0, ho);
+
 }
 
 void loop()
 {
-    lcd.print("     Hello"); // You can make spaces using well... spaces
-    lcd.setCursor(5, 1); // Or setting the cursor in the desired position.
-    lcd.print("World!");
-    delay(500);
-/*
-    // Flashing the backlight
-    for (int i = 0; i < 5; ++i)
-    {
-        lcd.backlight();
-        delay(50);
-        lcd.noBacklight();
-        delay(50);
-    }
 
-    lcd.backlight();
-    lcd.clear();
-    delay(500);
-    */
+  delay(500);
+  lcd.setCursor(4, 0);
+  lcd.print('2');
+  
+  delay(500);
+  lcd.setCursor(6, 0);
+  lcd.print('0');
+  
+  delay(500);
+  lcd.setCursor(8, 0);
+  lcd.print('1');
+  
+  delay(500);
+  lcd.setCursor(10, 0);
+  lcd.write(ho);
+
+
+
 }
---
+++ a_wiring_Vehicle/z_function_lcd_change.ino
@@ -0,0 +1,64 @@
+
+/*
+  함수 설명
+  LCD에 매개변수로 전달할 문자열을 출력하는 함수
+  
+  파라매터 설명
+  String text : LCD에 출력할 문자열
+  int cut :
+  0 >> 첫 줄에 문자열 전체 출력
+  양수 >> cut번째 글자에서 줄바꿈
+
+*/
+void LCD_print(String text, int cut){
+
+  lcd.clear();
+  int len = text.length(), location;
+
+  if( cut == 0){
+
+    location = ((len % 2) == 0)? (8 - len/2) : (7 - len/2); 
+
+    lcd.setCursor(location, 0);
+    lcd.print(text);
+
+  }
+  else if(cut > 0){
+    String str1, str2;
+    location;
+
+    str1 = text.substring(0, cut-1);
+    str2 = text.substring(cut, len);
+
+    //첫 번째 줄 출력 
+    len = str1.length();
+    location = ((len % 2) == 0)? (8 - len/2) : (7 - len/2); 
+    lcd.setCursor(location, 0);
+    lcd.print(str1);
+
+    //두 번째 줄 출력
+    len = str2.length();
+    location = ((len % 2) == 0)? (8 - len/2) : (7 - len/2);
+    lcd.setCursor(location, 1);
+    lcd.print(str2);
+  }
+}
+
+void LCD_print_S(String text, int col, int row, int cl = 0){
+  
+  if(cl == 1) lcd.clear();
+
+  lcd.setCursor(col, row);
+  lcd.print(text);
+
+}
+
+void LCD_print_C(char ch, int col, int row, int cl = 0){
+
+  if(cl == 1) lcd.clear();
+
+  lcd.setCursor(col, row);
+  lcd.print(ch);
+
+}
+
